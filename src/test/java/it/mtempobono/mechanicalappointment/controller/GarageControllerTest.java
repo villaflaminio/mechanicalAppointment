da98c230ac9dca5b537d95cf0a3e3f216488e8b0
villaflaminio <vifla01@gmail.com>
2023-05-01 09:56:57 +0200

    update garage

--- src/test/java/it/mtempobono/mechanicalappointment/controller/GarageControllerTest.java
+++ src/test/java/it/mtempobono/mechanicalappointment/controller/GarageControllerTest.java
@@ -29,6 +29,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -103,6 +104,71 @@ class GarageControllerTest {
 
         List<String> companyDifferences = MyReflectionTestUtils.compareGetters(garage, createGarage, compareGetters);
         assertThat(companyDifferences).isEmpty();
+
+        repository.delete(garage);
+    }
+
+
+    @Test
+    void should_update_one_garage() throws Exception {
+
+        //create company
+        final File createCompany = new ClassPathResource("mockData/garage/createGarage.json").getFile();
+        final String companyToCreate = Files.readString(createCompany.toPath());
+
+        MvcResult resultCompanyCreation = this.mockMvc.perform(post("/api/garages")
+                        .contentType(APPLICATION_JSON)
+                        .content(companyToCreate))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        JSONObject jsonResultCompanyCreation = (JSONObject) parser.parse(resultCompanyCreation.getResponse().getContentAsString());
+
+        Long garageId = Long.parseLong(jsonResultCompanyCreation.get("id").toString());
+        Garage oldGarage = repository.findById(garageId).orElseThrow(() -> new Exception("Garage not found"));
+
+        final File updateCompany = new ClassPathResource("mockData/garage/updateGarage.json").getFile();
+        final String companyToUpdate = Files.readString(updateCompany.toPath());
+
+        MvcResult result = this.mockMvc.perform(put("/api/garages/" + garageId)
+                        .contentType(APPLICATION_JSON)
+                        .content(companyToUpdate))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        JSONObject jsonObject = (JSONObject) parser.parse(content);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        GarageDto modifyGarageRequest = mapper.readValue(jsonObject.toString(), GarageDto.class);
+
+        Garage garage = repository.findById(Long.parseLong(jsonObject.get("id").toString())).get();
+
+        List<String> compareGetters = new ArrayList<>();
+        compareGetters.add("name");
+        compareGetters.add("address");
+        compareGetters.add("cap");
+        compareGetters.add("linkGoogleMaps");
+        compareGetters.add("latitude");
+        compareGetters.add("longitude");
+        compareGetters.add("phone");
+        compareGetters.add("email");
+        compareGetters.add("website");
+        compareGetters.add("logo");
+
+        //compare with modifyCompanyRequest
+        List<String> garageDifferences = MyReflectionTestUtils.compareGetters(garage, modifyGarageRequest, compareGetters);
+        assertThat(garageDifferences).isEmpty();
+        //compare with old company
+        garageDifferences = MyReflectionTestUtils.compareGetters(garage, oldGarage, compareGetters);
+        assertThat(garageDifferences).hasSize(10);
+
+        garageDifferences.clear();
+        compareGetters.clear();
 
         repository.delete(garage);
     }
