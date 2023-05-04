1c15a8f5c717d5e6830c17e9c755b1a20a15a723
villaflaminio <vifla01@gmail.com>
2023-05-04 19:04:55 +0200

    Vehicles tests

--- src/main/java/it/mtempobono/mechanicalappointment/model/dto/VehicleDto.java
+++ src/main/java/it/mtempobono/mechanicalappointment/model/dto/VehicleDto.java
@@ -1,7 +1,9 @@
 package it.mtempobono.mechanicalappointment.model.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
@@ -9,25 +11,27 @@ import java.io.Serializable;
  * A DTO for the {@link it.mtempobono.mechanicalappointment.model.entity.Vehicle} entity
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class VehicleDto implements Serializable {
     @Schema(description = "Unique identifier of the Vehicle.", example = "GJ123AA")
-    private final String plate;
+    private String plate;
 
     @Schema(description = "Model of the Vehicle.", example = "Urus")
-    private final String model;
+    private String model;
 
     @Schema(description = "Brand of the Vehicle.", example = "Lamborghini")
-    private final String brand;
+    private String brand;
 
     @Schema(description = "Year of the Vehicle.", example = "2020")
-    private final Integer year;
+    private Integer year;
 
     @Schema(description = "Fuel of the Vehicle.", example = "Gasoline")
-    private final String fuel;
+    private String fuel;
 
     @Schema(description = "Is the Vehicle active.", example = "true")
-    private final Boolean isActive;
+    private Boolean isActive;
 
     @Schema(description = "User id of the Vehicle.", example = "1")
-    private final Long userId;
+    private Long userId;
 }
